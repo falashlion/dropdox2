fc5771cbde59350e9a5d83f2f3cdf80f7d8c8b52
Fochu Felix Awah <60382233+falashlion@users.noreply.github.com>
2024-09-19 16:38:05 +0100

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -136,10 +136,10 @@ const HomePage = ({user, signOut}) => {
         >
               <div className="px-4 py-3" role="none">
                 <p className="text-sm text-gray-900 dark:text-white" role="none" onClick={<Overlay isVisible={isOverlayVisible} preview={"https://pagedone.io/asset/uploads/1704275541.png"} onClose={handleOverlayClose}/>}>
-                  {isUserData.fullName ||""}
+                  {isUserData?.fullName ||""}
                 </p>
                 <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                  {isUserData.email || ""}
+                  {isUserData?.email || ""}
                 </p>
               </div>
               <ul className="py-1" role="none">
